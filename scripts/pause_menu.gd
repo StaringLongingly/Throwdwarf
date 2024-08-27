ef672ef1b29176f364565a8995461d8f971c8755
StaringLongingly <74494805+StaringLongingly@users.noreply.github.com>
2024-08-28 02:59:35 +0300

    Trying to fix the broken web build

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -42,7 +42,6 @@ func animate_shader(delta: float, isIncreasing: bool) -> void:
 		LodProgress = clamp(LodProgress - delta / animationTime, 0, 1)
 	var LodProgressEased: float = clamp(ease(LodProgress, 0.6), 0, 1)
 		
-	rectangle.material.set_shader_parameter("lod", cachedLod * 0)
 	rectangle.material.set_shader_parameter("black", lerp(1.0, cachedBlack, LodProgressEased))
 
 # Function to reset shader parameters and cached variables
