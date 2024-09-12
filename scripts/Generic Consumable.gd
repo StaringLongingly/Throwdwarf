2d0a80058bb9d676f4616d3b4015ef75280ebad4
Longingly <74494805+StaringLongingly@users.noreply.github.com>
2024-09-13 00:56:22 +0300

    Refactored Generic Consumable with GPT o1

--- scripts/Generic Consumable.gd
+++ scripts/Generic Consumable.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+# Exported Variables
 @export var itemRarity: String
 @export var damage: float = 1
 @export var leech: float = 0
@@ -9,14 +10,8 @@ extends Node2D
 @export var isUsedByPlayer: bool
 @export var angleVariation: float = 0
 @export var rotateSpeed: float = 0
-var gotParried: bool = false
-var previousArtifactScene: PackedScene
-var artifactName: String
 
-var from: Vector2
-var to: Vector2
-var target_angle: float = 0 # For melee, bullet
-
+# Export Categories
 @export_category("Only for melee")
 @export var isRotating: bool = true
 @export var distanceFromBody: float = 2
@@ -24,7 +19,6 @@ var target_angle: float = 0 # For melee, bullet
 @export_category("Only for bullet")
 @export var bulletSpeed: float = 10
 @export var bulletPenetration: int = 0
-var isBulletMoving: bool = true
 
 @export_category("Only for mortar")
 @export var mortarDelay: float = 1
@@ -32,93 +26,121 @@ var isBulletMoving: bool = true
 
 @export_category("Only for Explosion")
 @export var animationDuration: float
-var animationProgress: float = 0
 
+# Internal Variables
+var gotParried: bool = false
+var previousArtifactScene: PackedScene
+var artifactName: String
+
+var from: Vector2
+var to: Vector2
+var target_angle: float = 0
+
+var animationProgress: float = 0
 var mortarProgress: float = 0
-var cachedScale
-var cachedPos
+var cachedScale: Vector2
+var cachedPos: Vector2
 
 var trail: Node2D
 var helmet: Node2D
 
+var isBulletMoving: bool = true
+var count: float = 0.0
+
+# Constants
+const MELEE = "melee"
+const MORTAR = "mortar"
+const BULLET = "bullet"
+const EXPLOSION = "explosion"
+
 func _ready() -> void:
-	artifactName = self.name
+	# Initialize artifact name
+	artifactName = name
 	print(artifactName)
+	
+	# Cache essential nodes
 	helmet = get_node("/root/Node2D/Player/Helmet")
-	var trailScene: PackedScene = preload("res://scenes/Item Trail.tscn")
-	trail = trailScene.instantiate()
+	
+	# Instantiate and configure trail
+	trail = preload("res://scenes/Item Trail.tscn").instantiate()
 	var rarityColor: Color = get_node("/root/Node2D/HUD").get_color(itemRarity)
 	trail.get_node("CPUParticles2D").color = Color(rarityColor, 0.5)
 	add_child(trail)
 	trail.position = Vector2.ZERO
 	
 	# Cache initial scale and position
-	global_scale = scale
-	cachedPos = position
 	cachedScale = scale
+	cachedPos = position
 	
-	# Handle initial setup based on weapon type
+	# Setup based on weapon type
 	match weaponType:
-		"melee":
+		MELEE:
 			scale = Vector2.ZERO
 			position = Vector2.ONE * 10000000
-		"explosion":
+		EXPLOSION:
 			get_node("Explosion Particles").emitting = true
 	
+	# Determine if used by player and set positions
 	isUsedByPlayer = not get_parent().is_in_group("Enemy")
-	get_to_from(isUsedByPlayer)
-	
-	# Set 'from' and 'to' positions based on whether it's used by player or enemy
+	set_from_to_positions(isUsedByPlayer)
 	
 	# Calculate target angle with variation
-	mortarProgress = 0
 	target_angle = (to - from).angle()
 	target_angle += (randf() * 2 - 1) * angleVariation / 360 * PI
 	
+	# Initialize position and rotation
 	global_position = from
 	rotation_degrees = rad_to_deg(target_angle) + 90
-	if weaponType == "melee":
+	
+	if weaponType == MELEE:
 		position = Vector2.UP * distanceFromBody * 1000
 	else:
 		reparent(get_node("/root"))
 		rotation_degrees = rad_to_deg(target_angle) + 90
 
-var count = 0
-
 func _process(delta: float) -> void:
 	count += rotateSpeed * delta
 	match weaponType:
-		"bullet":
-			rotation_degrees += rotateSpeed * delta
-			if isBulletMoving:
-				global_position += Vector2(1, 0).rotated(target_angle) * bulletSpeed * 1000 * delta
-		"mortar":
+		BULLET:
+			_process_bullet(delta)
+		MORTAR:
 			_process_mortar(delta)
-		"melee":
+		MELEE:
 			_process_melee(delta)
-		"explosion":
-			if not get_node("Explosion Particles").emitting:
-				destroy()
+		EXPLOSION:
+			_process_explosion()
+
+func _process_bullet(delta: float) -> void:
+	rotation_degrees += rotateSpeed * delta
+	if isBulletMoving:
+		var direction = Vector2.RIGHT.rotated(target_angle)
+		global_position += direction * bulletSpeed * 1000 * delta
 
 func _process_mortar(delta: float) -> void:
 	rotation_degrees += rotateSpeed * delta
-	if mortarProgress < 1:
-		mortarProgress += delta / mortarDelay
-	else:
-		var spawnedExplosion = explosionScene.instantiate()
-		spawnedExplosion.isUsedByPlayer = isUsedByPlayer  # Pass down the property
-		get_tree().root.add_child(spawnedExplosion)
-		spawnedExplosion.global_position = to 
-		destroy()
+	mortarProgress += delta / mortarDelay
 	
+	if mortarProgress >= 1:
+		_spawn_explosion()
+		return
+	
+	# Update scale and position with easing
 	var progress = ease(mortarProgress, -0.5)
 	var mortalProgressCos = mortarProgress * 2 - 1
-	scale = cachedScale * (sqrt(1 - mortalProgressCos * mortalProgressCos) + 0.4) 
+	scale = cachedScale * (sqrt(1 - mortalProgressCos * mortalProgressCos) + 0.4)
 	scale.y = pow(scale.y, 2.5)
 	global_position = lerp(from, to, progress)
 
-func _process_melee(_delta: float) -> void:
+func _spawn_explosion() -> void:
+	var spawnedExplosion = explosionScene.instantiate()
+	spawnedExplosion.isUsedByPlayer = isUsedByPlayer
+	get_tree().root.add_child(spawnedExplosion)
+	spawnedExplosion.global_position = to
+	destroy()
+
+func _process_melee(delta: float) -> void:
 	var newPosDif = Vector2.UP.rotated(deg_to_rad(count)) * distanceFromBody * 1000
+	
 	if not isUsedByPlayer:
 		newPosDif = newPosDif.rotated(target_angle)
 		if isRotating:
@@ -127,7 +149,7 @@ func _process_melee(_delta: float) -> void:
 			if count > 90 and scale.y > 0:
 				scale.y *= -1
 			rotation_degrees = 0
-	else :
+	else:
 		if isRotating:
 			rotation_degrees = count
 		else:
@@ -140,43 +162,58 @@ func _process_melee(_delta: float) -> void:
 	if count > 180:
 		destroy()
 
+func _process_explosion() -> void:
+	if not get_node("Explosion Particles").emitting:
+		destroy()
+
 func _on_area_2d_body_entered(body: Node) -> void:
 	var groupToCheck: String = "Enemy" if isUsedByPlayer else "Player"
-	if weaponType == "explosion":
+	
+	if weaponType == EXPLOSION:
 		groupToCheck = "Player" if isUsedByPlayer else "Enemy"
-		
+	
 	if body.is_in_group("Parry") and not gotParried and not is_in_group("Parry"):
-		gotParried = true
-		var self2: Node2D = self
-		var artifactString: String = "res://artifacts/scenes/" + artifactName + ".tscn"
-		print("Parried artifact: " + artifactString)
-		var artifactScene: PackedScene = load(artifactString)
-		var newArtifact = artifactScene.instantiate()
-		newArtifact.angleVariation += 20
-		newArtifact.bulletSpeed *= 2
-		newArtifact.DamageOverTimeDps /= 2 
-		newArtifact.damage /= 2
-
-		helmet.add_child(newArtifact)
-		newArtifact.global_position = global_position
-		destroy()
+		_handle_parry()
 	elif body.is_in_group(groupToCheck):
-		var scriptHost: Node2D = body.get_parent()
-		scriptHost.take_damage(damage, DamageOverTimeDps, DamageOverTimeDuration, leech)
-		if bulletPenetration == 0 and weaponType != "explosion":
-			destroy()
-		else:
-			bulletPenetration -= 1
-	else:
-		pass
+		_handle_damage(body, groupToCheck)
+	# No action for other cases
 
-func destroy():
+func _handle_parry() -> void:
+	gotParried = true
+	var artifactPath = "res://artifacts/scenes/%s.tscn" % artifactName
+	print("Parried artifact: " + artifactPath)
+	
+	var artifactScene: PackedScene = load(artifactPath)
+	var newArtifact = artifactScene.instantiate()
+	
+	# Modify artifact properties
+	newArtifact.angleVariation += 20
+	newArtifact.bulletSpeed *= 2
+	newArtifact.DamageOverTimeDps /= 2
+	newArtifact.damage /= 2
+	
+	# Attach to helmet
+	helmet.add_child(newArtifact)
+	newArtifact.global_position = global_position
+	destroy()
+
+func _handle_damage(body: Node, groupToCheck: String) -> void:
+	var scriptHost: Node2D = body.get_parent()
+	scriptHost.take_damage(damage, DamageOverTimeDps, DamageOverTimeDuration, leech)
+	
+	if bulletPenetration == 0 and weaponType != EXPLOSION:
+		destroy()
+	else:
+		bulletPenetration -= 1
+
+func destroy() -> void:
+	# Detach and stop trail particles
 	trail.reparent(get_node("/root/Node2D"))
 	trail.get_node("CPUParticles2D").emitting = false
 	queue_free()
 
-func get_to_from(isUsedByPlayer: bool):
-	if isUsedByPlayer:
+func set_from_to_positions(is_player: bool) -> void:
+	if is_player:
 		from = helmet.global_position
 		to = get_global_mouse_position()
 	else:
